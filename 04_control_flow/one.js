dc84b1abe692ee4d40331a3a45e581012f86af1b
tushar0408003 <tusharjakhar001@gmail.com>
2025-02-03 01:32:53 +0530

    Add files via upload

---
+++ 04_control_flow/one.js
@@ -0,0 +1,35 @@
+// const temprature = 40;
+
+// if(temprature==40){
+//     console.log("temprature less than 50");
+
+    
+// }
+
+// else{
+// console.log("temprature more than 50");
+// }
+
+// //<=,==,===
+
+const balance = 1000;
+if (balance<500){
+    console.log("less than 500");
+    }
+    else if(balance<750){
+        console.log("less than 750");
+        }
+        else if(balance <100){
+            console.log("less than 1000");
+            }
+            else{
+                    console.log("less than 1200");
+                    }
+        const UserloggedfrmEmail = true
+        const Userloggedfrmgoogle = false
+
+        if(UserloggedfrmEmail || Userloggedfrmgoogle){
+            console.log("user logged in");
+            
+        }
+            
---
+++ 04_control_flow/truty.js
@@ -0,0 +1,14 @@
+const UserloggedInEmail = []
+
+if(UserloggedInEmail){
+    console.log("got User email");
+    
+}
+else{
+    console.log("does not get User email");
+    
+}
+if(UserloggedInEmail.length === 0){
+    console.log("email is empty");
+    
+}
